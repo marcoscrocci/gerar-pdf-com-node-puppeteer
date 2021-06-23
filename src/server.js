9da66acba7689207a5a2510081e63d545d3e6128
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-06-22 21:58:11 -0300

    Header e Footer

--- src/server.js
+++ src/server.js
@@ -159,8 +159,56 @@ app.get('/lista/pdf', async(request, response) => {
 
     const pdf = await page.pdf({
         printBackground: true,
-        format: 'A4'
+        format: 'A4',
+        // margin: { top: "2.0cm" },
+        // displayHeaderFooter: true,
+        // headerTemplate: '<span style="font-size: 25px; margin-left: 1.5cm; margin-right: 1.5cm"> <span>HEADER</span>',
+        //footerTemplate: '<span style="font-size: 30px"> <span>FOOTER</span>'
+        
+        //landscape: true
     })
+
+    await browser.close()
+
+    response.contentType("application/pdf")
+
+    return response.send(pdf)
+})
+
+app.get('/testar/pdf', async(request, response) => {
+
+    const footer = `<footer style="margin: auto; width: 40%">
+            <p style="font-family: arial; float: right; width: 55%; color: red; margin-top: 24px; font-size: 10px">
+                <b>My footer</b>
+            </p>
+        </footer>`
+
+    const content = `<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charSet="utf-8"/>
+                <style type="text/css">
+                    body { backgroundColor: "red" }
+                </style>
+            </head>
+            <body>
+                <h1>Hello</h1>
+            </body>
+        </html>`
+
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.setContent(content, { waitUntil: "networkidle0" })
+    const pdf = await page.pdf({
+        format: "A4",
+        displayHeaderFooter: true,
+        //headerTemplate: '<span style="font-size: 30px"> <span class="pageNumber"></span> of <span class="totalPages"></span></span>',
+        headerTemplate: '<span style="font-size: 30px"> <span>HEADER</span>',
+        footerTemplate: footer,
+        margin: { top: "100px", bottom: "200px" },
+        printBackground: true,
+    })
+
 
     await browser.close()
 
